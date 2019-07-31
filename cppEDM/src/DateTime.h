f7a09a8e279abdec07d964703fe2d9a32ef43b68
cameronosmith <cos008@ucsd.edu>
2019-07-31 15:52:51 -0700

    azure build should work now

--- cppEDM/src/DateTime.h
+++ cppEDM/src/DateTime.h
@@ -1,18 +1,22 @@
 #ifndef DATETIMEUTIL_H
 #define DATETIMEUTIL_H
 
-#include <iostream>//for testing
-#include <cstdio> //time formatting
+#include <iostream> // for testing
+#include <cstdio>   // time formatting
 #include <ctime> 
 #include <string> 
 #include <regex> 
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
 
-const int iso_start_year    = 1900;
-const int iso_start_month   = 1;
+const int iso_start_year = 1900;
+const int iso_start_month = 1;
 
-struct datetime_info { struct tm    time = {};
-                       std::string  datetime_fmt;
-                       bool         unrecognized_fmt; };  
+struct datetime_info { struct tm   time;
+                       std::string datetime_fmt;
+                       bool        unrecognized_fmt; };
 
 // Prototypes
 void parse_datetime_str ( struct tm & time_obj, 
