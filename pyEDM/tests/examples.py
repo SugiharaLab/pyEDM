dc6afcba70336ecbabd9b56e3d7486ea7057e87b
SoftwareLiteracy <JosephPark@SoftwareLiteracyFoundation.org>
2020-01-22 15:36:52 -0500

    v1.2.0 cppEDM: Add positive tau. Default tau = -1. Set default knn if embedded = true.

--- pyEDM/tests/examples.py
+++ pyEDM/tests/examples.py
@@ -30,38 +30,38 @@ def main():
 
     # Note the path argument is empty "", file path is in Files{}
     df = EDM.EmbedDimension( "", Files[ "TentMap_rEDM.csv" ], None, "./", "",
-                             "1 100", "201 500", 10, 1, 1,
+                             "1 100", "201 500", 10, 1, -1,
                              "TentMap", "", False, False, 4 )
     
     df = EDM.PredictInterval( "", Files[ "TentMap_rEDM.csv" ], None, "./", "",
-                              "1 100", "201 500", 10, 2, 1,
+                              "1 100", "201 500", 10, 2, -1,
                               "TentMap", "", False, False, 4 );
 
     df = EDM.PredictNonlinear( "", Files[ "TentMapNoise_rEDM.csv" ], None,
-                               "./", "", "1 100", "201 500", "", 2, 1, 0, 1,
+                               "./", "", "1 100", "201 500", "", 2, 1, 0, -1,
                                "TentMap", "", False, False, 4 )
     
     # Tent map simplex : specify multivariable columns embedded = True
     df = EDM.Simplex( "", Files[ "block_3sp.csv" ], None, "./", "", 
-                      "1 99", "100 198", 3, 1, 0, 1, 0,
+                      "1 99", "100 198", 3, 1, 0, -1, 0,
                       "x_t y_t z_t", "x_t", True, False, True, True )
 
     # Tent map simplex : Embed column x_t to E=3, embedded = False
     df = EDM.Simplex( "", Files[ "block_3sp.csv" ], None, "./", "", 
-                      "1 99", "100 195", 3, 1, 0, 1, 0,
+                      "1 99", "100 195", 3, 1, 0, -1, 0,
                       "x_t", "x_t", False, False, True, True )
 
     M = EDM.Multiview( "", Files[ "block_3sp.csv" ], None, "./", "", 
-                       "1 100", "101 198", 3, 1, 0, 1,
+                       "1 100", "101 198", 3, 1, 0, -1,
                        "x_t y_t z_t", "x_t", 0, 0, False, 4, True )
 
     # S-map circle : specify multivariable columns embedded = True
     S = EDM.SMap( "", Files[ "circle.csv" ], None, "./", "", 
-                  "1 100", "101 198", 2, 1, 0, 1, 4, 0,
+                  "1 100", "101 198", 2, 1, 0, -1, 4, 0,
                   "x y", "x", "", "", True, False, True, True )
     
     df = EDM.CCM( "", Files[ "sardine_anchovy_sst.csv" ], None, "./", "", 
-                  3, 0, 0, 1, "anchovy", "np_sst",
+                  3, 0, 0, -1, "anchovy", "np_sst",
                   "10 70 10", 100, True, False, 0, True, True )
 
 #------------------------------------------------------------
