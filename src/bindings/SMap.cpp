53a1be71f38878d6ed5bdbc5bf1f9bfe8886deb9
SoftwareLiteracy <JosephPark@SoftwareLiteracyFoundation.org>
2020-03-01 07:11:41 -0500

    v1.3.0 Smap solver sklearn.

--- src/bindings/SMap.cpp
+++ src/bindings/SMap.cpp
@@ -18,9 +18,12 @@ std::valarray<double> SmapSolver( DataFrame< double >     A,
     // term in the LAPACK solver.  sklearn.linear_model's include an
     // intercept term by default.  Remove first column.
 
-    std::vector< size_t > non_bias_cols( A.NColumns() - 1 );
-    std::iota( non_bias_cols.begin(), non_bias_cols.end(), 1 );
-    A = A.DataFrameFromColumnIndex( non_bias_cols );
+    // JP NOTE: vector coeff should be size_t  On MS/python 3.5 this
+    // causes a pybind11 type mismatch... When we remove MS/python 3.5
+    // from the build set, revert this to the proper size_t.
+    std::vector< long unsigned int > coeff( A.NColumns() - 1 );
+    std::iota( coeff.begin(), coeff.end(), 1 );
+    A = A.DataFrameFromColumnIndex( coeff );
 
     // Convert DataFrame to pandas DataFrame / numpy array for sklearn 
     py::dict A_pydict = DFtoDict( DataFrameToDF( A ) );
