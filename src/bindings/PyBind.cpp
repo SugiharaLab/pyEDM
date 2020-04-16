e0e0820e8892a5cbaba244c273dbc2689b594df6
SoftwareLiteracy <JosephPark@SoftwareLiteracyFoundation.org>
2020-04-16 13:24:39 -0400

    v1.3.5 Multiview trainLib

--- src/bindings/PyBind.cpp
+++ src/bindings/PyBind.cpp
@@ -124,6 +124,7 @@ PYBIND11_MODULE( pyBindEDM, pyMod ) {
                py::arg("target")          = std::string(""),
                py::arg("multiview")       = 0,
                py::arg("exclusionRadius") = 0,
+               py::arg("trainLib")        = true,
                py::arg("verbose")         = false,
                py::arg("numThreads")      = 4 );
     
