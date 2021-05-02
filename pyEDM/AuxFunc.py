fa299bc9f7421fdec68ec94294682f0c80409ff8
SoftwareLiteracy <JosephPark@SoftwareLiteracyFoundation.org>
2021-05-02 11:15:29 +0200

    v1.8.1.1 Add SurrogateData function.

--- pyEDM/AuxFunc.py
+++ pyEDM/AuxFunc.py
@@ -3,6 +3,12 @@
 
 import pkg_resources
 
+from math   import floor, pi, sqrt, cos
+from cmath  import exp
+from random import sample, uniform, normalvariate
+
+from numpy             import mean, std, fft, arange, zeros, ptp
+from scipy.interpolate import UnivariateSpline
 from pandas            import DataFrame
 from matplotlib.pyplot import show, axhline
 
@@ -227,3 +233,125 @@ def NotStringIterable( obj ):
         else :
             return True
     return False
+
+#------------------------------------------------------------------------
+#
+#------------------------------------------------------------------------
+def SurrogateData( dataFrame     = None,
+                   column        = None,
+                   method        = 'ebisuzaki',
+                   numSurrogates = 10,
+                   alpha         = None,
+                   smooth        = 0.8,
+                   outputFile    = None ):
+    '''Three methods:
+
+    random_shuffle :
+      Sample the data with a uniform distribution.
+
+    ebisuzaki :
+      Journal of Climate. A Method to Estimate the Statistical Significance
+      of a Correlation When the Data Are Serially Correlated.
+      https://doi.org/10.1175/1520-0442(1997)010<2147:AMTETS>2.0.CO;2
+
+      Presumes data are serially correlated with low pass coherence. It is:
+      "resampling in the frequency domain. This procedure will not preserve
+      the distribution of values but rather the power spectrum (periodogram).
+      The advantage of preserving the power spectrum is that resampled series
+      retains the same autocorrelation as the original series."
+
+    seasonal :
+      Presume a smoothing spline represents the seasonal trend.
+      Each surrogate is a summation of the trend, resampled residuals,
+      and possibly additive Gaussian noise. Default noise has a standard
+      deviation that is the data range / 5.
+    '''
+
+    if dataFrame is None :
+        raise RuntimeError( "SurrogateData() empty DataFrame." )
+
+    if column is None :
+        raise RuntimeError( "SurrogateData() must specify column." )
+
+    # New dataFrame with initial time column
+    df = DataFrame( dataFrame.iloc[ :,0 ] )
+
+    if method.lower() == "random_shuffle" :
+        for s in range( numSurrogates ) : # use pandas sample here...
+            surr = dataFrame[ column ].sample( n = dataFrame.shape[0] ).values
+            df[ s + 1 ] = surr
+
+    elif method.lower() == "ebisuzaki" :
+        data          = dataFrame[ column ].values
+        n             = dataFrame.shape[0]
+        n2            = floor( n/2 )
+        mu            = mean   ( data )
+        sigma         = std    ( data )
+        a             = fft.fft( data )
+        amplitudes    = abs( a )
+        amplitudes[0] = 0
+
+        for s in range( numSurrogates ) :
+            thetas      = [ 2 * pi * uniform( 0, 1 ) for x in range( n2 - 1 )]
+            revThetas   = thetas[::-1]
+            negThetas   = [ -x for x in revThetas ]
+            angles      = [0] + thetas + [0] + negThetas
+            surrogate_z = [ A * exp( complex( 0, theta ) )
+                            for A, theta in zip( amplitudes, angles ) ]
+
+            if n % 2 == 0 : # even length
+                surrogate_z[-1] = complex( sqrt(2) * amplitudes[-1] *
+                                           cos( 2 * pi * uniform(0,1) ) )
+
+            ifft = fft.ifft( surrogate_z ) / n
+
+            realifft = [ x.real for x in ifft ]
+            sdevifft = std( realifft )
+
+            # adjust variance of surrogate time series to match original
+            scaled = [ sigma * x / sdevifft for x in realifft ]
+
+            df[ s + 1 ] = scaled
+
+    elif method.lower() == "seasonal" :
+        y = dataFrame[ column ].values
+        n = dataFrame.shape[0]
+
+        # Presume a spline captures the seasonal cycle
+        x      = arange( n )
+        spline = UnivariateSpline( x, y )
+        spline.set_smoothing_factor( smooth )
+        y_spline = spline( x )
+
+        # Residuals of the smoothing
+        residual = list( y - y_spline )
+
+        # spline plus shuffled residuals plus Gaussian noise
+        noise = zeros( n )
+
+        # If no noise specified, set std dev to data range / 5
+        if alpha is None :
+            alpha = ptp( y ) / 5
+
+        for s in range( numSurrogates ) :
+            noise = [ normalvariate( 0, alpha ) for z in range( n ) ]
+
+            df[ s + 1 ] = y_spline + sample( residual, n ) + noise
+
+    else :
+        raise RuntimeError( "SurrogateData() invalid method." )
+
+    df = df.round( 8 ) # Should be a parameter
+
+    # Rename columns
+    columnNames = [ column + "_" + str( c + 1 )
+                    for c in range( numSurrogates ) ]
+
+    columnNames.insert( 0, df.columns[0] ) # insert time column name
+
+    df.columns = columnNames
+
+    if outputFile :
+        df.to_csv( outputFile, index = False )
+
+    return df
