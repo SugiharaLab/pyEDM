155018526b8f4883d5449bc298d6421701437543
SoftwareLiteracy <JosephPark@SoftwareLiteracyFoundation.org>
2022-01-22 06:48:41 +0100

    v1.10.1 Sync cppEDM. CCM Tp validation. Parameter list numeric values.

--- src/bindings/DataFrame.cpp
+++ src/bindings/DataFrame.cpp
@@ -133,11 +133,64 @@ py::dict ParamMaptoDict( std::map< std::string, std::string > m ) {
     py::dict D;
 
     for ( auto pi = m.begin(); pi != m.end(); pi++ ) {
-        
-        // Why is cast required for string key but not valarray? 
-        D[ py::str( pi->first ) ] = pi->second;
+        // string types
+        if ( pi->first == "version"  or
+             pi->first == "method"   or pi->first == "columns" or
+             pi->first == "target"   or pi->first == "pathIn"  or
+             pi->first == "dataFile" or pi->first == "pathOut" or
+             pi->first == "predictOutputFile" or
+             pi->first == "SmapOutputFile"    or
+             pi->first == "blockOutputFile" ) {
+            // Why is cast required for string key but not valarray?
+            D[ py::str( pi->first ) ] = pi->second;
+        }
+        // int types
+        else if ( pi->first == "E"   or pi->first == "Tp"  or
+                  pi->first == "knn" or pi->first == "tau" or
+                  pi->first == "exclusionRadius"   or
+                  pi->first == "seed"              or
+                  pi->first == "subSamples"        or
+                  pi->first == "multiviewEnsemble" or
+                  pi->first == "multiviewD"        or
+                  pi->first == "generateSteps" ) {
+            D[ py::str( pi->first ) ] = std::stoi( pi->second );
+        }
+        // boolean types
+        else if ( pi->first == "randomLib"   or
+                  pi->first == "replacement" or
+                  pi->first == "includeData" or
+                  pi->first == "multiviewTrainLib"      or
+                  pi->first == "multiviewExcludeTarget" or
+                  pi->first == "embedded"      or
+                  pi->first == "const_predict" or
+                  pi->first == "parameterList" or
+                  pi->first == "verbose" ) {
+            if ( pi->second == "0" ) {
+                D[ py::str( pi->first ) ] = false;
+            }
+            if ( pi->second == "1" ) {
+                D[ py::str( pi->first ) ] = true;
+            }
+        }
+        // vector of int
+        else if ( pi->first == "lib"      or pi->first == "pred" or
+                  pi->first == "libSizes" or pi->first == "validLib" ) {
+            std::stringstream iss( pi->second );
+            std::vector< int > intVector;
+            int                value;
+
+            while ( iss >> value ) {
+                intVector.push_back( value );
+            }
+
+            D[ py::str( pi->first ) ] = intVector;
+        }
+        // float type
+        else if ( pi->first == "theta" ) {
+            D[ py::str( pi->first ) ] = std::stof( pi->second );
+        }
     }
-    
+
     return D;
 }
 
