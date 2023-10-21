41b724efda15eed830eac9ee026dc2e117eee5c8
SoftwareLiteracy <josephpark@softwareliteracyfoundation.org>
2023-10-21 07:51:32 -0700

    v1.15.0.4 Windog openblas lib

--- setup.py
+++ setup.py
@@ -200,7 +200,7 @@ Extension_modules = [
         extra_compile_args = ['-std=c++17'],
 
         # Note PEP 308: <expression1> if <condition> else <expression2>
-        libraries = ['EDM','openblas','gfortran','pthread','m','quadmath'] \
+        libraries = ['openblas','gfortran','pthread','m','quadmath','EDM'] \
                     if sys.platform.startswith('win') else ['EDM','lapack'],
         
         #extra_link_args = ["-static", "-static-libgfortran",
