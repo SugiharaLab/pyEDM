31d012c90e4d20b70e3bf4fb1aeb0c2d300cf846
SoftwareLiteracy <josephpark@softwareliteracyfoundation.org>
2023-10-22 17:18:53 -0700

    v1.15.0.4 Windog openblas lib

--- cppEDM/src/SMap.cc
+++ cppEDM/src/SMap.cc
@@ -490,11 +490,6 @@ void SMapClass::WriteOutput () {
     }
 }
 
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-// Do not use LAPACK on Windog: use scikit-learn LinearRegression
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#if !defined _WIN32 || defined USING_R
-
 //----------------------------------------------------------------
 // Singular Value Decomposition : wrapper for Lapack_SVD()
 //----------------------------------------------------------------
@@ -509,11 +504,11 @@ SVDValues SVD( DataFrame    < double > A,
 
     double *b = &( B[0] );
 
-    SVDValues SVD_ = Lapack_SVD( A.NRows(),    // number of rows
-                                 A.NColumns(), // number of columns
-                                 a,            // A
-                                 b,            // b
-                                 1.E-9 );      // rcond
+    SVDValues SVD_ =  Lapack_SVD( A.NRows(),     // number of rows
+                                  A.NColumns(),  // number of columns
+                                  a,             // A
+                                  b,             // b
+                                  1.E-9 );       // rcond
 
 #ifdef DEBUG_ALL
     std::cout << "SVD------------------------\n";
@@ -662,19 +657,3 @@ SVDValues Lapack_SVD( int     m, // rows in matrix
 
     return SVD_;
 }
-#else
-//-----------------------------------------------------------------
-// Singular Value Decomposition : SVD() dummy function: Replaced by 
-// scikit-learn LinearRegression on Windog. LAPACK is not feasible.
-//-----------------------------------------------------------------
-SVDValues SVD( DataFrame    < double > A,
-               std::valarray< double > B ) {
-    size_t nRow = A.NRows(); // Avoid compiler unused warn
-    size_t len  = B.size();  // Avoid compiler unused warn
-    SVDValues SVD_;
-    return SVD_;
-}
-#endif
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-// Do not use LAPACK on Windog: use scikit-learn LinearRegression
-//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
