a9d6b2cffcb2dc7de85cd22a7666955951f00c32
SoftwareLiteracy <josephpark@softwareliteracyfoundation.org>
2023-10-25 20:57:45 -0700

    v1.15.1.0 Replace openblas w sklearn

--- setup.py
+++ setup.py
@@ -200,11 +200,10 @@ Extension_modules = [
         extra_compile_args = ['-std=c++17'],
 
         # Note PEP 308: <expression1> if <condition> else <expression2>
-        libraries = ['EDM','pthread','m','quadmath'] \
-                    if sys.platform.startswith('win') else ['EDM','lapack'],
-        
-        extra_link_args = ["-static",
-                           "-static-libgfortran", "-static-libgcc"] \
+        libraries = ['EDM'] if sys.platform.startswith('win') \
+                            else ['EDM','lapack'],
+
+        extra_link_args = ["-static","-static-libgcc"] \
                           if sys.platform.startswith('win') else [],
     ),
 ]
