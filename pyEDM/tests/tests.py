c61f43944e8b2a1fb11231b65d1a909bddaf9609
SoftwareLiteracy <josephpark@softwareliteracyfoundation.org>
2023-11-30 13:18:31 -0800

    v1.15.3 CCM column names with space.

--- pyEDM/tests/tests.py
+++ pyEDM/tests/tests.py
@@ -22,7 +22,7 @@ class test_EDM( unittest.TestCase ):
     @classmethod
     def setUpClass( self ):
         self.GetDataFiles( self )
-        
+
     #------------------------------------------------------------
     # 
     #------------------------------------------------------------
@@ -39,8 +39,9 @@ class test_EDM( unittest.TestCase ):
                       "Multiview_combos_valid.csv",
                       "sardine_anchovy_sst.csv",
                       "CCM_anch_sst_cppEDM_valid.csv",
-                      "Smap_circle_noTime_valid.csv" ]
-        
+                      "Smap_circle_noTime_valid.csv",
+                      "columnNameSpace.csv" ]
+
         # Create map of module dataFiles pathnames in Files
         for file in dataFiles:
             filename = "data/" + file
@@ -55,9 +56,7 @@ class test_EDM( unittest.TestCase ):
     # Simplex
     #------------------------------------------------------------
     def test_simplex( self ):
-        #--------------------------------------------------------
-        # embedded = False
-        #--------------------------------------------------------
+        '''embedded = False'''
         print ( "--- Simplex embedded = False ---" )
         df = EDM.Simplex( "", self.Files[ "block_3sp.csv" ], None, "./", "", 
                           "1 100", "101 195", 3, 1, 0, -1, 0,
@@ -67,16 +66,14 @@ class test_EDM( unittest.TestCase ):
         # cppEDM and devEDM outputs are rounded to os.precision( 4 );
         dfv = EDM.ReadDataFrame( "",
                                  self.Files[ "Smplx_E3_block_3sp_pyEDM.csv" ] )
-        
+
         S1 =       dfv.get('Prediction_t(+1)')[1:95] # Skip row 0 Nan
         S2 = round( df.get('Predictions'), 4 )[1:95] # Skip row 0 Nan
         self.assertTrue( S1.equals( S2 ) )
 
     #------------------------------------------------------------
     def test_simplex2( self ):
-        #--------------------------------------------------------
-        # embedded = True
-        #--------------------------------------------------------
+        '''embedded = True'''
         print ( "--- Simplex embedded = True ---" )
         df = EDM.Simplex( "", self.Files[ "block_3sp.csv" ], None, "./", "", 
                           "1 99", "100 198", 3, 1, 0, -1, 0,
@@ -86,18 +83,36 @@ class test_EDM( unittest.TestCase ):
         # This has been rounded to os.precision( 4 );
         dfv = EDM.ReadDataFrame( "",
                                  self.Files["Smplx_embd_block_3sp_pyEDM.csv"] )
-        
+
         S1 =       dfv.get('Prediction_t(+1)')[1:98] # Skip row 0 Nan
         S2 = round( df.get('Predictions'), 4 )[1:98] # Skip row 0 Nan
         self.assertTrue( S1.equals( S2 ) )
-        
+
+    #------------------------------------------------------------
+    def test_simplex3( self ):
+        '''dataFrame column names with space'''
+        print ( "--- Simplex column names with space ---" )
+        df = EDM.Simplex( "", self.Files[ "columnNameSpace.csv" ],
+                          None, "./", "", 
+                          "1 80", "81 85", 5, 1, 0, -1, 0,
+                          "Var 1,Var 2", "Var 5 1",
+                          False, False, True, False, [], 0, False )
+
+    #------------------------------------------------------------
+    def test_simplex4( self ):
+        '''dataFrame column names vector with space'''
+        print ( "--- Simplex column names with space ---" )
+        df = EDM.Simplex( "", self.Files[ "columnNameSpace.csv" ],
+                          None, "./", "", 
+                          "1 80", "81 85", 5, 1, 0, -1, 0,
+                          ['Var 1','Var 2'], "Var 5 1",
+                          False, False, True, False, [], 0, False )
+
     #------------------------------------------------------------
     # S-map
     #------------------------------------------------------------
     def test_smap( self ):
-        #--------------------------------------------------------
-        # circle test embedded = True
-        #--------------------------------------------------------
+        '''circle test embedded = True'''
         print ( "--- S-map circle embedded = True ---" )
         dfc = EDM.ReadDataFrame( "", self.Files[ "circle.csv" ] )
 
@@ -106,20 +121,18 @@ class test_EDM( unittest.TestCase ):
                        "1 100", "101 198", 2, 1, 0, -1, 4, 0,
                        "x y", "x", "", "",
                        None, True, False, False, False, [], True, 0, False )
-        
+
         df = SM['predictions']
-        
+
         dfv = EDM.ReadDataFrame( "", self.Files[ "Smap_circle_pyEDM.csv" ] )
-        
+
         S1 =       dfv.get('Prediction_t(+1)')
         S2 = round( df.get('Predictions'), 4 ) 
         self.assertTrue( S1.equals( S2 ) )
 
     #------------------------------------------------------------
     def test_smap2( self ):
-        #--------------------------------------------------------
-        # block_3sp embedded = True
-        #--------------------------------------------------------
+        '''block_3sp embedded = True'''
         print ( "--- S-map block_3sp embedded = True ---" )
         SM = EDM.SMap( "", self.Files[ "block_3sp.csv" ], None, "./", "", 
                        "1 99", "100 198", 3, 1, 0, -1, 2, 0,
@@ -137,13 +150,11 @@ class test_EDM( unittest.TestCase ):
 
     #------------------------------------------------------------
     def test_smap3( self ):
-        #--------------------------------------------------------
-        # circle noTime = True
-        #--------------------------------------------------------
+        '''S-map circle noTime = True'''
         print ( "--- S-map circle noTime = True ---" )
         dfcirc = read_csv( self.Files[ "circle.csv" ] )
         dfcirc = dfcirc[['x']]
-        
+
         SM = EDM.SMap( "", "", dfcirc, "./", "", 
                        "1 100", "101 190", 2, 1, 0, -1, 2, 0,
                        "x", "x", "", "", 
@@ -159,6 +170,28 @@ class test_EDM( unittest.TestCase ):
         S2 = round( df.get('Predictions'), 4 ) 
         self.assertTrue( S1.equals( S2 ) )
 
+    #------------------------------------------------------------
+    def test_smap4( self ):
+        '''S-map space in column names'''
+        print ( "--- S-map space in column names ---" )
+        SM = EDM.SMap( "", self.Files[ "columnNameSpace.csv" ],
+                       None, "./", "", 
+                       "1 80", "81 85", 5, 1, 0, -1, 2, 0,
+                       "Var 2,", "Var 5 1,", "", "", 
+                       None, False, False, False, False, [],
+                       True, 0, False, False, True )
+        
+    #------------------------------------------------------------
+    def test_smap5( self ):
+        '''S-map space in vector column names'''
+        print ( "--- S-map space in vector column names ---" )
+        SM = EDM.SMap( "", self.Files[ "columnNameSpace.csv" ],
+                       None, "./", "", 
+                       "1 80", "81 85", 5, 1, 0, -1, 2, 0,
+                       ['Var 1','Var 2'], "Var 5 1,", "", "", 
+                       None, True, False, False, False, [],
+                       True, 0, False, False, True )
+        
     #------------------------------------------------------------
     # Multiview
     #------------------------------------------------------------
@@ -200,6 +233,24 @@ class test_EDM( unittest.TestCase ):
                                  noTime = True )
 
         self.assertTrue( dfv.equals( round( df, 4 ) ) )
+
+    #------------------------------------------------------------
+    def test_ccm2( self ):
+        '''CCM column names with space'''
+        print ( "--- CCM column names with space ---" )
+        df = EDM.CCM( "", self.Files[ "columnNameSpace.csv" ],
+                      None, "./", "", 
+                      3, 0, 0, -1, 0, "Var 1,Var 2", "Var 5 1",
+                      "20 50 90", 10, False, False, 0, False, False, False )
+
+    #------------------------------------------------------------
+    def test_ccm3( self ):
+        '''CCM column vector names with space'''
+        print ( "--- CCM column vector names with space ---" )
+        df = EDM.CCM( "", self.Files[ "columnNameSpace.csv" ],
+                      None, "./", "", 
+                      3, 0, 0, -1, 0, ['Var 1','Var 2'], ['Var 5 1','Var3'],
+                      "20 50 90", 10, False, False, 0, False, False, False )
 
 #------------------------------------------------------------
 #
