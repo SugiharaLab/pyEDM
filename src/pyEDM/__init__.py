36dc04937fe558113277596699f52fe4d2558196
SoftwareLiteracy <josephpark@softwareliteracyfoundation.org>
2026-07-10 18:27:57 -0700

    v2.5.5 Move apps/ to src/

--- src/pyEDM/__init__.py
+++ src/pyEDM/__init__.py
@@ -6,6 +6,10 @@ from .API      import PredictExclusionRadius
 from .AuxFunc  import Examples, PlotObsPred, PlotCoeff, ComputeError
 from .AuxFunc  import SurrogateData
 from .LoadData import sampleData
+# EDM applications
+from .apps.CrossMap_Matrix  import CrossMap_Matrix
+from .apps.CCM_Matrix       import CCM_Matrix
+from .apps.EmbedDim_Columns import EmbedDim_Columns
 
-__version__     = "2.5.4"
-__versionDate__ = "2026-07-06"
+__version__     = "2.5.5"
+__versionDate__ = "2026-07-10"
